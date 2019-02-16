03018405e3d839cb36f524ba2983968f1a81f896
komjit <47686559+komjit@users.noreply.github.com>
2019-02-16 10:21:43 +0100

    Create SzamlahegyServiceProvider.php

---
+++ src/KomjIT/Szamlahegy/SzamlahegyServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace KomjIT\Szamlahegy;
+
+class SzamlahegyServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Bootstraps the application services.
+     */
+    public function boot()
+    {
+        
+    }
+
+    /**
+     * Registers the application services.
+     */
+    public function register()
+    {
+        
+    }
+}
